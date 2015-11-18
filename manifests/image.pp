a9e06ab2fe1e5d676c7efd3f1627194f8a4b8ac1
Julien Kassar <github@kassisol.com>
2015-11-18 11:52:28 -0500

    Update image define

--- manifests/image.pp
+++ manifests/image.pp
@@ -4,7 +4,7 @@
 define docker::image (
   $ensure = 'present',
   $image  = $name,
-  $tag    = undef,
+  $tag    = 'latest',
 ) {
 
   validate_re($ensure, [ '^present$', '^absent$' ],
@@ -13,13 +13,8 @@ define docker::image (
   validate_string($image)
   validate_string($tag)
 
-  if $tag != undef {
-    $image_real = "${image}:${tag}"
-    $image_cond = "tags=`docker images | awk '/${image}/ && match(\$1, /^${image}$/) { print \$2 }'` && exist='false' && for tag in \${tags} ; do if [ \${tag} == \"${tag}\" ] ; then exist='true' ; fi ; done && test \${exist} == 'true'"
-  } else {
-    $image_real = $image
-    $image_cond = "docker images | awk '/${image}/ && match(\$1, /^${image}$/) { print \$1 }'"
-  }
+  $image_real = "${image}:${tag}"
+  $image_cond = "images=`docker images | grep \"${image}\" | awk 'BEGIN{ OFS=\":\" } { print \$1, \$2 }'` && exist='false' && for img in \${images} ; do if [ \${img} == \"${image_real}\" ]; then exist='true' ; fi ; done && test \${exist} == 'true'"
 
   if $ensure == 'present' {
     exec { "docker_image_${name}_pull":
