e674ef540c7fcfe77f0e64817ed068aaa6f8f014
Julien Kassar <github@kassisol.com>
2017-06-08 08:58:49 -0400

    Move registry to conf

--- manifests/conf/registry.pp
+++ manifests/conf/registry.pp
@@ -1,8 +1,8 @@
-# == Class: docker::registry::artifactory
+# == Class: docker::conf::registry
 #
-# Configure Artifactory credentials
+# Configure registry credentials
 #
-class docker::registry::artifactory (
+class docker::conf::registry (
   $ensure = 'present',
   $host   = 'MANDATORY',
   $token  = 'MANDATORY',
@@ -10,18 +10,18 @@ class docker::registry::artifactory (
 ) {
 
   validate_re($ensure, [ '^present$', '^absent$' ],
-    'docker::registry::artifactory::ensure is invalid and does not match the regex.')
+    'docker::conf::registry::ensure is invalid and does not match the regex.')
 
   if $host == 'MANDATORY' or empty($host) {
-    fail('docker::registry::artifactory::host is MANDATORY.')
+    fail('docker::conf::registry::host is MANDATORY.')
   }
   validate_fqdn($host)
 
   if $token == 'MANDATORY' or empty($token) {
-    fail('docker::registry::artifactory::token is MANDATORY.')
+    fail('docker::conf::registry::token is MANDATORY.')
   }
   validate_re($token, '^[A-Za-z0-9=]*$',
-    'docker::registry::artifactory::token is invalid and does not match the regex.')
+    'docker::conf::registry::token is invalid and does not match the regex.')
 
   if $email != undef or $email != '' {
     validate_email($email)
@@ -30,7 +30,7 @@ class docker::registry::artifactory (
   file { 'root_dockercfg':
     ensure  => present,
     path    => '/root/.dockercfg',
-    content => template('docker/dockercfg_artifactory.erb'),
+    content => template('docker/dockercfg.erb'),
     owner   => 'root',
     group   => 'root',
     mode    => '0644',
